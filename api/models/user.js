609678396eb3cad774b2c7d8a69d7fe29a4b6946
Wabuluka <dwabuluka@gmail.com>
2019-08-23 11:25:29 +0300

    [ft #168047526] user can view a mentor

--- api/models/user.js
+++ api/models/user.js
@@ -12,7 +12,7 @@ class User{
             this.bio = bio;
             this.occupation = occupation;
             this.expertise = expertise;
-            this.status = status;
+            this.status = "available";
             this.isMentor = "false";
             this.createdOn = moment().format('LLLL')
             this.lastModified = moment().format('LLLL');
