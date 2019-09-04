fee2e2896d7c13240eda3fdba9532b697c49038c
Wabuluka <dwabuluka@gmail.com>
2019-09-04 07:55:59 +0300

    Improve homepage [Finishes #167972368]

--- ui/assets/js/custom.js
+++ ui/assets/js/custom.js
@@ -1,20 +1,3 @@
-var modal = document.getElementById("myModal");
-var btn = document.getElementById("myBtn");
-var span = document.getElementsByClassName("close")[0];
-btn.onclick = function() {
-  modal.style.display = "block";
-}
-span.onclick = function() {
-  modal.style.display = "none";
-}
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-}
-
-
-
 function myFunction(){
     var x = document.getElementById("myTopnav");
     if (x.className === "top-navigation") {
@@ -23,10 +6,6 @@ function myFunction(){
       x.className = "top-navigation";
     }
 }
-
-
-
-
 
 function openPage(pageName, elmnt, color) {
   var i, tabcontent, tablinks;
